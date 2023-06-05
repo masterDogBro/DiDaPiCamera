548396d74e5d0c74370bbcd17f516e2c795ae627
masterDogBro <1583581477@qq.com>
2023-06-05 22:49:50 +0800

    Fix: fix the error in the bash script

--- DiDaCamera.sh
+++ DiDaCamera.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 
 
-DIR=/home/rtos/proxy
-export PYTHONPATH=/home/rtos
+DIR=/home/rtos/measure/DiDaPiCamera
+cd $DIR
+
 export DEVICE=Dida
+export PYTHONPATH=$(dirname ${DIR})
 
 sudo kill -9 `ps -aux | grep ffmpeg_udp_send | awk '{print $2}'`
 sudo kill -9 `ps -aux | grep camera_client | awk '{print $2}'`
 
-cd $DIR
-export DEVICE=Dida
-export PYTHONPATH=$(dirname ${DIR})
-
-python3 ${DIR}/camera_client.py
+nohup python3 ${DIR}/camera_client.py &
 sleep 3s
 python3 ${DIR}/ffmpeg_udp_send.py
